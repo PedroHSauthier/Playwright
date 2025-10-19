5c2536c49fbc276b06fec0c7cfca33f057fe15e5
pedrohsauthier <phsauthier@yahoo.com>
2025-10-19 20:46:38 -0300

    Como utilizar a barra de pesquisas com press_sequentially

--- testes/test_certa_parametros.py
+++ testes/test_certa_parametros.py
@@ -6,8 +6,17 @@ load_dotenv()
 cpf = os.getenv("cliente_cpf")
 senha = os.getenv("cliente_senha")
 
-def baixar_relatorios(logar_usuario):
+def test_baixar_relatorios(logar_usuario):
     
-    pagina_logada = logar_usuario(cpf, senha)
+    pl = logar_usuario(cpf, senha)
     
+    menu = pl.locator("#menusistema")
+    menu.get_by_role("textbox").click()
+    menu.get_by_role("textbox").press_sequentially("loja", delay=2)
+    
+    item_loja = pl.get_by_text("Loja de Relatorios", exact=True)
+    expect(item_loja).to_be_visible()
+    item_loja.click()
+    
+    pl.screenshot(path="screenshots/certa/relatorios_baixar.png")
     
