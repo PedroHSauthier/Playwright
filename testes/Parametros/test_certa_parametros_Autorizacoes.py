fea029dbcc5e6d73a1c4b994876147f6f43e7a3f
PedroHSauthier <phsauthier@yahoo.com>
2025-11-04 08:16:05 -0300

    Teste autorizações completo

--- testes/Parametros/test_certa_parametros_Autorizacoes.py
+++ testes/Parametros/test_certa_parametros_Autorizacoes.py
@@ -136,13 +136,13 @@ def test_modificar_parametros_autorizacoes_filaencaminhamento(logar_usuario_cert
     popup_vacina = page.locator(".dx-overlay-content.dx-popup-normal.dx-resizable:visible")
     expect(popup_vacina).to_be_visible(timeout=5000)
     expect(popup_vacina.get_by_role("textbox")).to_be_visible()
-    popup_vacina.get_by_role("textbox").fill("OUTROS PROCEDIMENTOS - A4")
-    expect(popup_vacina.get_by_text("OUTROS PROCEDIMENTOS - A4", exact=True)).to_be_visible()
-    popup_vacina.get_by_text("OUTROS PROCEDIMENTOS - A4", exact=True).click()
+    popup_vacina.get_by_role("textbox").fill("FILA AUTORIZAÇÃO")
+    expect(popup_vacina.get_by_text("FILA AUTORIZAÇÃO", exact=True)).to_be_visible()
+    popup_vacina.get_by_text("FILA AUTORIZAÇÃO", exact=True).click()
     expect(popup_vacina).to_be_hidden()
     
-    # 2. Modifica o "Modelo outros procedimentos".
-    campo_vacina = page.get_by_label("Modelo outros procedimentos", exact=True)
+    # 2. Modifica o "Modelo fila encaminhamento".
+    campo_vacina = page.get_by_label("Modelo fila encaminhamento", exact=True)
     expect(campo_vacina).to_be_visible()
     campo_vacina.scroll_into_view_if_needed()
     campo_vacina.wait_for(state="visible", timeout=3000)
@@ -152,9 +152,9 @@ def test_modificar_parametros_autorizacoes_filaencaminhamento(logar_usuario_cert
     popup_vacina = page.locator(".dx-overlay-content.dx-popup-normal.dx-resizable:visible")
     expect(popup_vacina).to_be_visible(timeout=5000)
     expect(popup_vacina.get_by_role("textbox")).to_be_visible()
-    popup_vacina.get_by_role("textbox").fill("OUTROS PROCEDIMENTOS - A4")
-    expect(popup_vacina.get_by_text("OUTROS PROCEDIMENTOS - A4", exact=True)).to_be_visible()
-    popup_vacina.get_by_text("OUTROS PROCEDIMENTOS - A4", exact=True).click()
+    popup_vacina.get_by_role("textbox").fill("FILA ENCAMINHAMENTO")
+    expect(popup_vacina.get_by_text("FILA ENCAMINHAMENTO", exact=True)).to_be_visible()
+    popup_vacina.get_by_text("FILA ENCAMINHAMENTO", exact=True).click()
     expect(popup_vacina).to_be_hidden()
 
     # Salva as alterações e verifica a mensagem de sucesso.
